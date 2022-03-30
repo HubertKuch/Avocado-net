a8a6e6d48366f78b0c0c17f48221e8c935cde54a
hubert <kuchhubert@gmail.com>
2022-03-30 09:59:37 +0200

    Main view

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,23 @@
 import React from "react";
+import avocado from "../images/avocado.svg";
+import ColorText from "../components/ColorText";
+import Nav from "../components/Nav";
 
 export default function Main() {
-    return <>
-    <div>test</div></>
+    return (
+        <>
+            <header>
+                <img src={avocado} alt="avocado-icon" id={"logo"}/>
+                <span id={"project-name"}>Avocado</span>
+            </header>
+            <main>
+                <p id={"project-description"}>
+                    <ColorText color={'#92C58A'}>Avocado</ColorText> is hobbyist PHP framework which implements routing and
+                    object-relational mapping (ORM). If you want use <ColorText color={"#5179DE"}>PHP</ColorText> only to backend
+                    <ColorText color={'#92C58A'}>Avocado</ColorText> is best position to you.
+                </p>
+            </main>
+            <Nav/>
+        </>
+    )
 }

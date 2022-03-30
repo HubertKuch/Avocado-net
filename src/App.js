db7eaef285730875f41d7d2f7d2b2b26b65721ee
hubert <kuchhubert@gmail.com>
2022-03-30 09:59:22 +0200

    Standard routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<Main/>} />
+                <Route path={"/"} exact element={<Main/>} />
+                <Route path={"/documentation/:slug"} element={<Main/>} />
+                <Route path={"/installation"} element={<Main/>} />
+                <Route path={"/about-project"} element={<Main/>} />
+                <Route path={"/author"} element={<Main/>} />
+                <Route path={"/suggest"} element={<Main/>} />
+                <Route path={"/initializer"} element={<Main/>} />
             </Routes>
         </BrowserRouter>
     )
